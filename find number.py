9bf1d725434433a1548f30aca446bddfe0c4b4ba
esenkeldieveldar292-rgb <esenkeldieveldar292@gmail.com>
2026-01-11 21:25:05 +0500

    guess_number_game.py

---
+++ find number.py
@@ -0,0 +1,18 @@
+import random
+secret_number = random.randint(1,20)
+
+attempts = 0
+while True:
+
+    guess = int(input("Taxmini son kiriting>> "))
+    attempts += 1
+    if guess == secret_number:
+        print(f"To'g'ri toptingiz, urinishlar soni {attempts}")
+        break
+    elif guess < secret_number:
+        print("Ko'proq son kiriting ")
+    elif guess > secret_number:
+        print("Kamroq son kiriting")
+    if guess < 1  or guess >20:
+        print("Faqat 1 dan 10 gacha bo'lgan sonlarni kiriting")
+        continue
